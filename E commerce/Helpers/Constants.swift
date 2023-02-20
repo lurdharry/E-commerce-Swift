efb2868a2be89a351d11c811d7c9739709d73060
lurdharry <azeezhammed57@gmail.com>
2023-02-20 12:14:24 -0500

    chore: add constants

---
+++ E commerce/Helpers/Constants.swift
@@ -0,0 +1,27 @@
+//
+//  Constants.swift
+//  E commerce
+//
+//  Created by Hammed on 20/02/2023.
+//
+
+import Foundation
+
+
+// firebase Headers
+struct FirebaseHeaders {
+    static let USER_PATH = "User"
+    static let CATEGORY_PATH = "Category"
+    static let ITEMS_PATH = "Items"
+    static let BASKET_PATH = "Basket"
+
+}
+
+//Category
+struct Constants {
+    static let NAME = "name"
+    static let IMAGENAME = "imageName"
+    static let OBJECTID = "objectId"
+}
+
+ 
